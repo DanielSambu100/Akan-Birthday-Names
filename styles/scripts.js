a8f998545921cf9939ee3d1a171f65f8a5151664
Danielsambu100 <daniel.wanyonyi@student.moringaschool.com>
2022-03-07 23:59:00 +0300

    update1 on JS file

--- styles/scripts.js
+++ styles/scripts.js
@@ -1,5 +1,21 @@
-var CC, YY, MM, DD, d, dayValue;
-var dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
-var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
-var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
-​
+// var CC, YY, MM, DD, d, dayValue;
+// var dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
+// var maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
+// var femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
+// ​
+
+const button = document.querySelector("#button")
+const gender = document.querySelector("#selection")
+
+const dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
+const maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
+const femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
+
+button.addEventListener("click",function(){
+  const firstInput = document.querySelector("#input1").value
+
+  let dateOfBirth = new Date(firstInput)
+  let myDay = dateOfBirth.getDay();
+
+  alert(`${myDay}`)
+})
