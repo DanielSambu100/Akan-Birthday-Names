cddbed06b0b19adaeff3a67b53fdc94af89bfebe
Danielsambu100 <daniel.wanyonyi@student.moringaschool.com>
2022-03-08 08:41:13 +0300

    update2 on js file

--- styles/scripts.js
+++ styles/scripts.js
@@ -5,17 +5,28 @@
 // ​
 
 const button = document.querySelector("#button")
-const gender = document.querySelector("#selection")
+const gender = document.querySelectorAll("#selection")
+const output = document.querySelector("#message")
 
 const dayNames = ["Sunday","Monday","Tuesday","Wednesday", "Thursday", "Friday","Saturday" ];
 const maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw", "Kofi","Kwame"];
 const femaleNames = ["Akosua","Adwoa","Abenaa","Akua"," Yaa","Afua","Ama"];
 
-button.addEventListener("click",function(){
+button.addEventListener("click",function(evt){
+  evt.preventDefault()
   const firstInput = document.querySelector("#input1").value
 
   let dateOfBirth = new Date(firstInput)
   let myDay = dateOfBirth.getDay();
 
-  alert(`${myDay}`)
+  // alert(`${myDay}`)
+  for(let choice of gender){
+    let myGender = choice.value;
+    if(myGender==="male"){
+      output.innerHTML = `Your Akan name is ${maleNames[myDay]}`
+    }
+    else if(myGender==="female"){
+      output.innerHTML = `Your Akan name is ${femaleNames[myDay]}`
+    }
+  }
 })
